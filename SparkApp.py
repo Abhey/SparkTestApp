a10ab7de79d311f28ef25b448467a16ea162cbe6
Abhey Rana <abheyranacool123.ar@gmail.com>
2019-04-23 21:09:18 +0530

    Fixed absolute file path

--- SparkApp.py
+++ SparkApp.py
@@ -1,13 +1,16 @@
 """SparkApp.py"""
 from pyspark import SparkContext
+import os
 
-dataFile = "./data.txt"
+dir = os.path.dirname(os.path.realpath(__file__))
+
+dataFile = dir + "/data.txt"
 sparkContext = SparkContext("local", "SparkApp")
 data = sparkContext.textFile(dataFile).cache()
 
 numAs = data.filter(lambda s: 'a' in s).count()
 numBs = data.filter(lambda s: 'b' in s).count()
 
-f = open("output.txt", "a")
+f = open(dir + "/output.txt", "a")
 f.write("Lines with a: {0}, lines with b: {1}".format(numAs, numBs))
 f.close()
